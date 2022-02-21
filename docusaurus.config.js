d6b07ba077765f78d7747c48dbcd9cfc652638e8
Steffen Vogel <post@steffenvogel.de>
2022-02-21 09:04:58 +0100

    use proper favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/logos/villas_framework.svg',
   organizationName: 'VILLASframework',
   projectName: 'Documentation',
 
