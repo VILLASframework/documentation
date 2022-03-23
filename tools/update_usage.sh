62373face4d2eadd7adce877adad2ec5c40a7bbf
Steffen Vogel <post@steffenvogel.de>
2022-03-23 17:37:56 +0100

    remove color from usage

--- tools/update_usage.sh
+++ tools/update_usage.sh
@@ -5,5 +5,5 @@ PREFIX=villas
 TOOLS=$(compgen -c | egrep "^${PREFIX}-" | sort -u)
 
 for TOOL in ${TOOLS}; do
-    ${TOOL} -h > ${TOOL}.txt
+    ${TOOL} -h | sed 's/\x1b\[[0-9;]*m//g' > ${TOOL}.txt
 done
